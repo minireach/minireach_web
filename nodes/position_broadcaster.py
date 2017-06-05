d8570e29a5fbca616feaaf08a532df7386ecb3e7
mseln <magse761@student.liu.se>
2017-06-05 21:29:20 +0200

    Add position and rotation to map

--- nodes/position_broadcaster.py
+++ nodes/position_broadcaster.py
@@ -11,30 +11,26 @@ from math import pi
 from minireach_world_state.srv import *
 from std_srvs.srv import *
 
-class PalletBroadcaster():
+class TruckBroadcaster():
 
     def __init__(self):
-		rospy.init_node('storage_location_broadcaster', anonymous=True)
-		self.tf_listener = tf.TransformListener()
-                rospy.Service('set_storage_location', Empty, self.setDeadReckoningGoal)
-                rospy.Service('clear_storage_location', Empty, self.clearDeadReckoningGoal)
+        rospy.init_node('storage_location_broadcaster', anonymous=True)
+        self.tf_listener = tf.TransformListener()
 
-		self.found = False
-		self.pos_x = 0
-		self.pos_y = 0
+        self.pos_x = 0
+        self.pos_y = 0
+        self.pos_z = 0.1
+        self.rot = None
 
+        self.initMarker()
+        self.point = Point(self.pos_x, self.pos_y, self.pos_z)
+        rate = rospy.Rate(10.0)
+        while not rospy.is_shutdown():
+            self.updatePosition()
+            self.publishMarker()
+            rate.sleep()
 
-		self.initMarkers()
-		self.point = Point(self.pos_x, self.pos_y, 0)
-		self.markers.points.append(self.point)
-		rate = rospy.Rate(10.0)
-		while not rospy.is_shutdown():
-                        self.setDeadReckoningGoal()
-                        self.publishMarkers()
-                        self.broadcastPalletPosition()
-			rate.sleep()
-
-    def setDeadReckoningGoal(self):
+    def updatePosition(self):
         rospy.loginfo("Setting location")
         try:
             self.tf_listener.waitForTransform('/map','/base_footprint', rospy.Time(0), rospy.Duration(20.0))
@@ -43,31 +39,15 @@ class PalletBroadcaster():
             self.pos_y = trans[1]
 
             euler = euler_from_quaternion(rot)
-            self.rot_z = euler[2]
+            self.rot = rot
 
-            self.found = True
-            rospy.loginfo("Location in map x: %f, y: %f; rotoation z: %f", self.pos_x, self.pos_y, self.rot_z)
+            rospy.loginfo("Location in map x: %f, y: %f", self.pos_x, self.pos_y)
 
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            rospy.loginfo("tf lookup for laser_link failed")
-            self.found = False
+            rospy.loginfo("tf lookup for base_footprint failed")
 
 
-    def clearDeadReckoningGoal(self, empty):
-        self.found = False
-        return EmptyResponse()
-
-
-    def broadcastPalletPosition(self):
-        br = tf.TransformBroadcaster()
-        pallet_quaternion = quaternion_from_euler(0, 0, self.rot_z)
-        br.sendTransform((self.pos_x, self.pos_y, 0),
-                            pallet_quaternion,
-                            rospy.Time.now(),
-                            'storage_location',
-                            'map')
-
-    def initMarkers(self):
+    def initMarker(self):
         marker_scale = 0.3
         marker_lifetime = 0.5 # 0 if forever
         marker_ns = 'storage'
@@ -76,31 +56,36 @@ class PalletBroadcaster():
 
         self.marker_pub = rospy.Publisher('position_marker', Marker, queue_size=5)
 
-        self.markers = Marker()
-        self.markers.ns = marker_ns
-        self.markers.id = marker_id
-        self.markers.type = Marker.CUBE_LIST
-        self.markers.action = Marker.ADD
-        self.markers.lifetime = rospy.Duration(marker_lifetime)
-        self.markers.scale.x = marker_scale
-        self.markers.scale.y = marker_scale
-        self.markers.color.r = marker_color['r']
-        self.markers.color.g = marker_color['g']
-        self.markers.color.b = marker_color['b']
-        self.markers.color.a = marker_color['a']
+        self.marker = Marker()
+        self.marker.ns = marker_ns
+        self.marker.id = marker_id
+        self.marker.type = Marker.CUBE
+        self.marker.action = Marker.ADD
+        self.marker.lifetime = rospy.Duration(marker_lifetime)
+        self.marker.scale.x = marker_scale
+        self.marker.scale.y = marker_scale
+        self.marker.color.r = marker_color['r']
+        self.marker.color.g = marker_color['g']
+        self.marker.color.b = marker_color['b']
+        self.marker.color.a = marker_color['a']
 
-        self.markers.header.frame_id = 'map'
-        self.markers.header.stamp = rospy.Time.now()
-        self.markers.points = list()
+        self.marker.header.frame_id = 'map'
+        self.marker.header.stamp = rospy.Time.now()
+        self.marker.points = list()
 
-    def publishMarkers(self):
-        self.markers.points[0].x = self.pos_x
-        self.markers.points[0].y = self.pos_y
-        self.marker_pub.publish(self.markers)
+    def publishMarker(self):
+        self.marker.pose.position.x = self.pos_x
+        self.marker.pose.position.y = self.pos_y
+        self.marker.pose.position.z = self.pos_z
+        self.marker.pose.orientation.x = self.rot[0]
+        self.marker.pose.orientation.y = self.rot[1]
+        self.marker.pose.orientation.z = self.rot[2]
+        self.marker.pose.orientation.w = self.rot[3]
+        self.marker_pub.publish(self.marker)
 
 
 if __name__ == '__main__':
     try:
-        PalletBroadcaster()
+        TruckBroadcaster()
     except rospy.ROSInterruptException:
         rospy.loginfo("Pallet Broadcaster finished")
