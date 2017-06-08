e9fa666b8ae0f303b6289aa86d193435134757fe
Samuel Lindgren <samuel.lindgren@toyota-industries.eu>
2017-06-08 21:02:16 +0200

    Add pallet visualizer and publish as MarkerArray

--- nodes/pallet_visualizer.py
+++ nodes/pallet_visualizer.py
@@ -3,7 +3,7 @@
 import rospy
 import tf
 import math
-from visualization_msgs.msg import Marker
+from visualization_msgs.msg import Marker, MarkerArray
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Point, Quaternion, Twist
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
@@ -16,80 +16,59 @@ class TruckBroadcaster():
 
     def __init__(self):
         rospy.init_node('storage_location_broadcaster', anonymous=True)
-        self.tf_listener = tf.TransformListener()
-
-        self.pos_x = 0
-        self.pos_y = 0
-        self.pos_z = 0.1
-        self.rot = None
-
-        self.initMarker()
-        self.point = Point(self.pos_x, self.pos_y, self.pos_z)
-        rate = rospy.Rate(10.0)
-
         self.pallet_sub = rospy.Subscriber('/canvas_ar', AlvarMarkers, self.palletCallback)
+        self.marker_pub = rospy.Publisher('position_marker', MarkerArray, queue_size=5)
 
-        while not rospy.is_shutdown():
-            self.updatePosition()
-            #self.publishMarker()
-            rate.sleep()
-
-    def updatePosition(self):
-        rospy.loginfo("Setting location")
-        try:
-            self.tf_listener.waitForTransform('/map','/base_footprint', rospy.Time(0), rospy.Duration(20.0))
-            (trans, rot) = self.tf_listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
-            self.pos_x = trans[0]
-            self.pos_y = trans[1]
-
-            euler = euler_from_quaternion(rot)
-            self.rot = rot
-
-            rospy.loginfo("Location in map x: %f, y: %f", self.pos_x, self.pos_y)
-
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            rospy.loginfo("tf lookup for base_footprint failed")
+        rospy.spin()
 
     def palletCallback(self, pallets_message):
+        markers = []
         for pallet in pallets_message.markers:
-            self.publishMarker(pallet.pose.pose)
+            markers.append(pallet.pose.pose)
+        rospy.loginfo("Publishing " + str(len(markers)) + "markers.")
+        self.publishMarkers(markers)
 
 
 
-    def initMarker(self):
+    def initMarker(self, id=0):
         marker_scale = 0.3
         marker_lifetime = 0.5 # 0 if forever
         marker_ns = 'storage'
-        marker_id = 0
+        marker_id = id
         marker_color = {'r': 0.0, 'g': 0.0, 'b': 1.0, 'a': 1.0}
 
-        self.marker_pub = rospy.Publisher('position_marker', Marker, queue_size=5)
 
-        self.marker = Marker()
-        self.marker.ns = marker_ns
-        self.marker.id = marker_id
-        self.marker.type = Marker.CUBE
-        self.marker.action = Marker.ADD
-        self.marker.lifetime = rospy.Duration(marker_lifetime)
-        self.marker.scale.x = marker_scale
-        self.marker.scale.y = marker_scale
-        self.marker.color.r = marker_color['r']
-        self.marker.color.g = marker_color['g']
-        self.marker.color.b = marker_color['b']
-        self.marker.color.a = marker_color['a']
+        marker = Marker()
+        marker.ns = marker_ns
+        marker.id = marker_id
+        marker.type = Marker.CUBE
+        marker.action = Marker.ADD
+        marker.lifetime = rospy.Duration(marker_lifetime)
+        marker.scale.x = marker_scale
+        marker.scale.y = marker_scale
+        marker.color.r = marker_color['r']
+        marker.color.g = marker_color['g']
+        marker.color.b = marker_color['b']
+        marker.color.a = marker_color['a']
 
-        self.marker.header.frame_id = 'map'
-        self.marker.header.stamp = rospy.Time.now()
-        self.marker.points = list()
+        marker.header.frame_id = 'map'
+        marker.header.stamp = rospy.Time.now()
+        marker.points = list()
+
+        return marker
 
 
-    def publishMarker(self, marker_pose):
-        self.marker.pose = marker_pose
-        self.marker_pub.publish(self.marker)
+    def publishMarkers(self, marker_poses):
+        markers = MarkerArray()
+        for id, marker_pose in enumerate(marker_poses):
+            marker = self.initMarker(id=id)
+            marker.pose = marker_pose
+            markers.markers.append(marker)
+        self.marker_pub.publish(markers)
 
 
 if __name__ == '__main__':
     try:
         TruckBroadcaster()
     except rospy.ROSInterruptException:
-        rospy.loginfo("Pallet Broadcaster finished")
+        rospy.loginfo("Pallet Broadcaster finished"py.spin()
